eab6c45a9ecfae860d1b016b20bbcad6a0ef25ec
FitnessTaco <61722660+FitnessTaco@users.noreply.github.com>
2025-05-22 22:16:19 -0700

    make strings nullable

--- DataMapper/DataMapperConsole/SampleModel.cs
+++ DataMapper/DataMapperConsole/SampleModel.cs
@@ -10,8 +10,8 @@ public class SampleClass
 
     [MapTo("Id")]
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
